26b78ec40bfa135479504168542a272ab33015d9
poll0ui5 <pl.ledoux@yahoo.fr>
2025-11-17 12:44:41 +0100

    Ajout des classes du TD7

---
+++ PROJET_LO02/src/package1/Couleur.java
@@ -0,0 +1,6 @@
+package package1;
+
+public enum Couleur {
+	TREFLE, CARREAU, COEUR,PIQUE
+}
+
---
+++ PROJET_LO02/src/package1/Valeur.java
@@ -0,0 +1,6 @@
+package package1;
+
+public enum Valeur {
+	SEPT, HUIT, NEUF, DIX, VALET, DAME, ROI, AS
+}
+

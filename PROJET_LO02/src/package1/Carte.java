cc98075445e55056e5a1091d2f639e0ac146aba0
poll0ui5 <pl.ledoux@yahoo.fr>
2025-11-17 11:27:51 +0100

    Création du projet JAVA et ajout de la classe Carte

---
+++ PROJET_LO02/src/package1/Carte.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package package1;
+
+/**
+ * 
+ */
+public class Carte {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
